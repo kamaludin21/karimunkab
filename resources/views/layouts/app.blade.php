843bd1212c8952ec70eff7e59c56a430ceaa6761
Kamaludin <kamal.zyel@gmail.com>
2025-06-29 22:58:20 +0700

    berita pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,19 +22,104 @@
 
 </head>
 
-<body class="bg-slate-200">
-  <nav class="bg-white py-2 flex justify-between padding-x-wrapper">
-    <div class="flex gap-4">
-      <img src="{{ asset('assets/images/logo_kab.png') }}" class="w-16 h-auto" alt="logo_kab">
-      <img src="{{ asset('assets/images/logo_hut.png') }}" class="w-16 h-auto" alt="logo_hut">
+<body class="grid bg-slate-100">
+  <nav class="bg-white sticky top-0 z-10 shadow">
+    <div class=" max-w-screen-lg px-2 lg:px-0 flex justify-between mx-auto w-full py-4">
+      <div class="flex gap-6">
+        <img src="{{ asset('assets/images/logo_kab.png') }}" class="w-12 h-auto hover:scale-110 duration-200"
+          alt="logo_kab">
+        <img src="{{ asset('assets/images/logo_hut.png') }}" class="w-12 h-auto hover:scale-110 duration-200"
+          alt="logo_hut">
+      </div>
+      <div class="bg-white w-1/3 h-10 border border-slate-400 rounded-lg flex p-1">
+        <input type="text" class="flex-1 focus:outline-none pl-2" placeholder="Pencarian">
+        <button class="bg-white hover:bg-amber-300 rounded-r-md px-2">
+          <svg xmlns="http://www.w3.org/2000/svg" class="text-orange-600 h-6 w-auto" viewBox="0 0 24 24" fill="none"
+            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+            <path d="M21 21l-6 -6" />
+          </svg>
+        </button>
+      </div>
     </div>
-    <div>
-      <p>bg-search {$search-box}</p>
+    {{-- Menu navigator --}}
+    <div class="max-w-screen-lg px-2 lg:px-0 mx-auto w-full border-t-1 border-slate-200">
+      <ul class="text-sm font-medium uppercase flex gap-4 text-slate-500">
+        <li class="h-full">
+          <a href="/"
+            class="{{ ($activePage ?? '') === 'beranda' ? 'text-orange-600' : 'hover:text-slate-700  border-transparent' }} flex gap-1 items-center border-b-2 py-2">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-5 w-5">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path
+                d="M9 3a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-4a2 2 0 0 1 -2 -2v-6a2 2 0 0 1 2 -2zm0 12a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-4a2 2 0 0 1 -2 -2v-2a2 2 0 0 1 2 -2zm10 -4a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-4a2 2 0 0 1 -2 -2v-6a2 2 0 0 1 2 -2zm0 -8a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-4a2 2 0 0 1 -2 -2v-2a2 2 0 0 1 2 -2z" />
+            </svg>
+            <span>Beranda</span>
+          </a>
+        </li>
+        <li>
+          <a href="/berita"
+            class="{{ ($activePage ?? '') === 'berita' ? 'text-orange-600 border-b-2' : 'hover:text-slate-700 border-transparent border-b-2' }} h-full block hover:border-b-2 py-2">
+            Berita
+          </a>
+        </li>
+        <li class="py-2">
+          Informasi Publik
+        </li>
+
+        <li class="py-2">
+          Pariwisata
+        </li>
+        <li class="py-2">
+          PPID
+        </li>
+        <li class="py-2">
+          Sekapur Sirih
+        </li>
+      </ul>
     </div>
   </nav>
-  <main>
+  <main class="bg-white">
     @yield('content')
   </main>
-</body>
+  <footer class="w-full bg-slate-900 py-10">
+    <div class="max-w-screen-lg mx-auto text-slate-300">
+      {{-- <p>Bumi</p>
+      <p>Berazam</p> --}}
+      <div class="">
+        {{-- Line 1 --}}
+        <div class="flex py-8 border-b border-slate-500">
+          <div class="flex-1">
+            <p class="leading-8">Quick Link</p>
+            <ul class="text-sm font-light leading-6">
+              <li>Umpan Balik</li>
+              <li>Peta Situs</li>
+            </ul>
+          </div>
+          <div class="flex-1">
+            <p class="leading-8">Legalitas</p>
+            <ul class="text-sm font-light leading-6">
+              <li>Ketentuan Layanan</li>
+              <li>Kebijakan Privasi</li>
+              <li>Kebijakan Cookie</li>
+            </ul>
+          </div>
+          <div class="w-2/4">
+            <p class="text-9xl tracking-wide font-bold text-right text-slate-50">BUMI</p>
+          </div>
+        </div>
 
-</html>
+        {{-- Line 2 --}}
+        <div class="flex py-8 items-end">
+          <div class="flex-1">
+            <p class="text-slate-200 text-base font-normal">KABUPATEN KARIMUN</p>
+            <p class="text-sm font-light">DISKOMINFO &#169; 2025 </p>
+          </div>
+          <p class="w-3/4 leading-[0.8] text-9xl tracking-wide font-bold text-right text-slate-50">
+            <span class="align-[0px]">BERAZAM</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</body>
---
+++ resources/views/pages/berita/index.blade.php
@@ -0,0 +1,100 @@
+@extends('layouts.app', ['activePage' => 'berita'])
+
+@section('content')
+  <div class=" max-w-screen-lg mx-auto w-full">
+    {{-- Hero Section --}}
+    <section class="flex gap-8 items-center py-16">
+      <div class="flex-1 w-1/2">
+        <img
+          src="https://medusajs.com/_next/image/?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2F5a711ubd%2Fproduction%2F57c48878a2a1d44d37a44bc145200c9402af4fe1-3200x1672.jpg%3Fw%3D1280%26fm%3Dpng&w=3840&q=75"
+          class="w-full h-auto cover rounded-lg ring-1 ring-zinc-300 shadow-md hover:shadow-lg" alt="">
+      </div>
+      <div class="flex-1 grid gap-4 place-content-center justify-start">
+        <div class="flex items-center gap-2 text-slate-600 h-fit">
+          <p>21 Mei 2025</p>
+          <x-icons.dot class="w-2 h-2" />
+          <p>Pemerintahan</p>
+        </div>
+        <h1 class="font-medium text-5xl leading-[1.1] text-slate-600 line-clamp-3">
+          <span>Fastest Way to Build and Deploy Medusa Applications</span>
+
+          <!-- Inline action button -->
+          <a href="/berita/slug"
+            class="inline-flex items-center justify-center align-middle
+            ring-2 ring-slate-600 p-2 rounded-full group
+            hover:bg-black transition duration-200 ml-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+              stroke="currentColor"
+              class="h-5 w-5 text-slate-600 group-hover:text-slate-100 group-hover:rotate-45 transition duration-200">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+            </svg>
+          </a>
+        </h1>
+      </div>
+    </section>
+    {{-- Hero Section --}}
+
+
+    <section class="w-full slate-100 border-t-1 border-slate-200 py-16">
+      <div class="max-w-screen-lg grid gap-8 mx-auto w-full">
+        {{-- Header Filter --}}
+        <div class="flex justify-between">
+          <div class="flex gap-4 items-center text-slate-500 font-medium">
+            <div class="py-1 px-3 bg-white border border-slate-200 rounded-full text-orange-600">Semua Berita</div>
+            <div>Pilih Kategori</div>
+            <div>Berita OPD</div>
+          </div>
+          <div>
+            {{-- Searchbar --}}
+          </div>
+        </div>
+        {{-- Header Filter --}}
+
+
+        <div class="grid grid-cols-3 gap-14">
+          @for ($i = 0; $i < 9; $i++)
+            <div class="hover:slate-200 rounded-lg duration-200 grid group">
+              <img
+                src="https://medusajs.com/_next/image/?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2F5a711ubd%2Fproduction%2F8cc4fc064087bb7539d41e7847b3fe8fd893a5e4-3200x1672.jpg%3Fw%3D600&w=3840&q=75"
+                class="w-full h-auto min-h-32 duration-200 object-cover group-hover:object-fill rounded-lg ring-1 ring-zinc-300 shadow-md hover:shadow-lg"
+                alt="">
+              <div class="text-sm flex items-center gap-2 pt-3 pb-1 text-slate-600 h-fit">
+                <p>21 Mei 2025</p>
+                <x-icons.dot class="w-1 h-1" />
+                <p>Pemerintahan</p>
+              </div>
+              <a href="/berita/slug"
+                class="text-lg font-medium text-slate-600 hover:underline underline-offset-2 cursor-pointer hover:text-orange-600">
+                Lorem ipsum dolor sit amet
+                consectetur adipisicing elit.</a>
+            </div>
+          @endfor
+        </div>
+
+
+        {{-- Pagination --}}
+        <div class="flex border-t border-slate-200 pt-6 justify-between items-center">
+          <ul class="flex gap-4 text-lg font-medium">
+            <li class="px-2 bg-orange-600 text-white cursor-pointer rounded-lg hover:bg-slate-200">1</li>
+            <li class="px-2 cursor-pointer rounded-lg hover:bg-slate-200">2</li>
+            <li class="px-2 cursor-pointer rounded-lg hover:bg-slate-200">3</li>
+            <li class="px-2 cursor-pointer rounded-lg hover:bg-slate-200">...</li>
+          </ul>
+
+          <div>
+            <button class="" disabled>
+              Sebelumnya
+            </button>
+            <button>
+              Selanjutnya
+            </button>
+          </div>
+
+        </div>
+        {{-- Pagination --}}
+
+      </div>
+    </section>
+
+  </div>
+@endsection
