e24a126bf7be42482925cf09b9d153b77070e9b5
Kamaludin <kamal.zyel@gmail.com>
2025-07-25 14:43:20 +0700

    fix overlap when small screen

--- resources/views/components/sections/berita.blade.php
+++ resources/views/components/sections/berita.blade.php
@@ -5,7 +5,7 @@
       <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div
           class="h-72 md:h-96 bg-[url(https://images.unsplash.com/photo-1750232453488-f6a40815126d?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover grid items-end">
-          <div class="bg-slate-800/50 p-4 h-full flex flex-col w-full justify-between">
+          <div class="bg-slate-800/50 p-4 h-full flex flex-col gap-2 w-full justify-between">
             <div>
               <div class="w-fit whitespace-nowrap p-0.5 rounded-md text-slate-100 text-sm">
                 <p>12 Januari 2025 | Politik</p>
@@ -27,7 +27,7 @@
             </div>
           </div>
         </div>
-        <div class="h-full w-full grid grid-cols-2 gap-4 auto-rows-fr">
+        <div class="h-full w-full grid grid-cols-2 gap-2 md:gap-4 auto-rows-fr">
           <div class="bg-slate-800 col-span-2 h-full w-full flex">
             <div
               class="w-1/2 bg-cover h-full bg-[url(https://images.unsplash.com/photo-1747134392051-5e112c58ce1e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)]">
@@ -56,16 +56,15 @@
           </div>
           <div
             class="h-full w-full bg-[url(https://images.unsplash.com/photo-1749906891532-2ab9f9aeef74?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxN3x8fGVufDB8fHx8fA%3D%3D)] bg-cover grid items-end">
-            <div class="bg-slate-800/50 p-4 h-full flex flex-col w-full justify-between">
+            <div class="bg-slate-800/50 p-4 h-full flex flex-col gap-2 w-full justify-between">
               <div>
-                <div class="w-fit whitespace-nowrap p-0.5 rounded-md text-slate-100 text-sm">
+                <div class="w-fit p-0.5 rounded-md text-slate-100 text-sm">
                   <p>12 Januari 2025 | Politik</p>
                 </div>
                 <a href="/slug"
                   class="hover:underline underline-offset-2 text-xl leading-[1.2] font-light hover:text-white text-slate-100 line-clamp-2">Lorem
                   ipsum dolor sit amet consect with etur adipisicing elit. Laudantium voluptate officiis possimus?</a>
               </div>
-
               <div class="flex justify-end">
                 <a href="/berita/slug"
                   class="ring-1 hover:bg-white ring-white p-1.5 rounded-full group duration-200 cursor-pointer">
@@ -80,9 +79,9 @@
           </div>
           <div
             class="h-full w-full bg-[url(https://images.unsplash.com/photo-1749741331500-04c330bc53a9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D)] bg-cover grid items-end">
-            <div class="bg-slate-800/50 p-4 h-full flex flex-col w-full justify-between">
+            <div class="bg-slate-800/50 p-4 h-full flex gap-2 flex-col w-full justify-between">
               <div>
-                <div class="w-fit whitespace-nowrap p-0.5 rounded-md text-slate-100 text-sm">
+                <div class="w-fit p-0.5 rounded-md text-slate-100 text-sm">
                   <p>12 Januari 2025 | Politik</p>
                 </div>
                 <a href="/slug"
