7011a4c88258fa25a64e848b9ba04f4e72aa1433
Kamaludin <kamal.zyel@gmail.com>
2025-08-06 11:07:40 +0700

    allow user at prod

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+
+class User extends Authenticatable implements FilamentUser
 {
   /** @use HasFactory<\Database\Factories\UserFactory> */
   use HasFactory, Notifiable;
@@ -44,6 +47,12 @@ class User extends Authenticatable
       'email_verified_at' => 'datetime',
       'password' => 'hashed',
     ];
+  }
+
+  public function canAccessPanel(Panel $panel): bool
+  {
+    // return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+    return true;
   }
 
   public function news()
