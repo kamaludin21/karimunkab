219df2a4feccf859fd5a08c382e7fd0041251631
Kamaludin <kamal.zyel@gmail.com>
2025-08-13 13:28:18 +0700

    put more button at bottom

--- resources/views/components/sections/nomor-penting.blade.php
+++ resources/views/components/sections/nomor-penting.blade.php
@@ -1,25 +1,11 @@
 @props(['phones'])
 
-<section class="w-full bg-white py-10 md:py-20">
+<section class="w-full bg-white pb-10 pt-0 md:pt-20">
   <div class="max-w-screen-lg dot-pattern mx-auto grid gap-6 rounded-none md:rounded-xl pt-16 pb-2 md:pb-4 px-2 md:px-4">
-    <p class="text-5xl font-medium text-slate-50 text-center">Nomor Penting</p>
-    <p class="text-xl font-light text-white w-2/3 mx-auto text-center">
+    <p class="text-3xl md:text-5xl font-medium text-slate-50 text-center">Nomor Penting</p>
+    <p class="text-xl font-light text-white w-full md:w-2/3 mx-auto text-center">
       Informasi kontak berikut disediakan untuk layanan penting. Pastikan penggunaannya tepat dan bertanggung jawab.
     </p>
-
-    <div
-      class="flex items-center gap-4 before:h-px before:flex-1 before:bg-slate-700 after:h-px after:flex-1 after:bg-slate-700">
-      <a href="/nomor-penting"
-        class="text-slate-300 hover:text-white hover:bg-slate-600 cursor-pointer border border-slate-300 w-fit mx-auto px-3 py-1 rounded-full flex gap-1 items-center">
-        <span>Nomor Lainnya</span>
-        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
-          stroke-linejoin="round" class="h-5 w-5">
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path
-            d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
-        </svg>
-      </a>
-    </div>
 
     <div class="grid grid-cols-2 md:grid-cols-3 gap-2 md:gap-4 mt-4 md:mt-8">
       @foreach ($phones as $phone)
@@ -50,6 +36,20 @@
           </div>
         </div>
       @endforeach
+    </div>
+
+    <div
+      class="flex items-center gap-4 before:h-px before:flex-1 before:bg-slate-700 after:h-px after:flex-1 after:bg-slate-700">
+      <a href="/nomor-penting"
+        class="text-slate-300 hover:text-white hover:bg-slate-600 cursor-pointer border border-slate-300 w-fit mx-auto px-3 py-1 rounded-full flex gap-1 items-center">
+        <span>Nomor Lainnya</span>
+        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+          stroke-linejoin="round" class="h-5 w-5">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path
+            d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+        </svg>
+      </a>
     </div>
   </div>
 </section>
