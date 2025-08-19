ceee296d96b9cf9f484587b2b5647b85226fe119
Kamaludin <kamal.zyel@gmail.com>
2025-08-19 23:42:56 +0700

    merah putih gradient

--- resources/views/components/sections/hero.blade.php
+++ resources/views/components/sections/hero.blade.php
@@ -7,7 +7,7 @@
     class="relative z-10 flex flex-col lg:flex-row items-start lg:items-center px-2 justify-between max-w-screen-lg mx-auto w-full gap-4 bg-transparent">
     <div class="flex-1 w-full">
       <p
-        class="w-full text-[18vw] lg:text-[9rem] font-extrabold bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.orange.600),theme(colors.orange.200),theme(colors.orange.600),theme(colors.amber.600),theme(colors.orange.600),theme(colors.orange.200),theme(colors.orange.600))] bg-[length:200%_auto] animate-gradient tracking-wider leading-none flex justify-between">
+        class="w-full text-[18vw] lg:text-[9rem] font-extrabold bg-clip-text text-transparent bg-[conic-gradient(from_90deg_at_0%_50%,_white,_red)] tracking-wider leading-none flex justify-between">
         <span>K</span>
         <span>A</span>
         <span>R</span>
@@ -16,6 +16,16 @@
         <span>U</span>
         <span>N</span>
       </p>
+      {{-- <p
+        class="w-full text-[18vw] lg:text-[9rem] font-extrabold bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.orange.600),theme(colors.orange.200),theme(colors.orange.600),theme(colors.amber.600),theme(colors.orange.600),theme(colors.orange.200),theme(colors.orange.600))] bg-[length:200%_auto] animate-gradient tracking-wider leading-none flex justify-between">
+        <span>K</span>
+        <span>A</span>
+        <span>R</span>
+        <span>I</span>
+        <span>M</span>
+        <span>U</span>
+        <span>N</span>
+      </p> --}}
     </div>
     <div
       class="pl-0 lg:pl-8 flex-0 text-5xl md:text-4xl grid gap-2 font-medium text-slate-100 border-t-2 lg:border-t-0 border-l-0 lg:border-l-2 border-orange-600 pt-4 lg:pt-0">
