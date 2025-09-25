d37203130c5b74fd1508a775a00c8b7f42738578
Kamaludin <kamal.zyel@gmail.com>
2025-09-25 09:11:47 +0700

    fix dynami url

--- resources/js/share.js
+++ resources/js/share.js
@@ -7,8 +7,7 @@ export const share = {
     popupCenter(navUrl, "Facebook Share", 480, 320);
   },
   x(pageUrl) {
-    // const api = "https://x.com/intent/post?url=";
-    const api = "https://x.com/intent/tweet?text=https://karimunkab.go.id/berita/baca/upacara-hari-perhubungan-nasional-2025-di-kabupaten-karimun-dipimpin-wabup-rocky-marciano-bawole";
+    const api = "https://x.com/intent/post?url=";
     const navUrl = api + encodeURIComponent(pageUrl);
     popupCenter(navUrl, "X Share", 480, 320);
   },
