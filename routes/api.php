9a90f649d63b87de39045804788282bfcecc0c42
Kamaludin <kamal.zyel@gmail.com>
2025-09-26 10:32:18 +0700

    reinstall api

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DeployController;
 
-// Route::get('/phpinfo', function () {
-//     return phpinfo();
-// });
-
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 Route::post('/webhook/deploy', [DeployController::class, 'handle']);
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DeployController;
 
 // Route::get('/phpinfo', function () {
 //     return phpinfo();
 // });
-
-// Route::post('/webhook/deploy', [DeployController::class, 'handle']);
