b8ced3ef06e4256a324b809c46dad5f2a89b3221
Kamaludin <kamal.zyel@gmail.com>
2025-09-26 10:37:22 +0700

    remove middleware

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DeployController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 Route::post('/webhook/deploy', [DeployController::class, 'handle']);
