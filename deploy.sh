dd36a9de09700b62a76b35d8356e4cd143c3e1cd
Kamaludin <kamal.zyel@gmail.com>
2025-09-26 10:17:15 +0700

    minimize jobs

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,5 @@ APP_DIR="/var/www/karimunkab"
 cd $APP_DIR || exit
 
 git pull
-
 php artisan optimize:clear
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-php artisan icons:cache
-
 npm run build
