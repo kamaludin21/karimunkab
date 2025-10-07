443ac4092796423ff65c1ed8285f5b6dae372fdc
Kamaludin <kamal.zyel@gmail.com>
2025-10-07 10:07:15 +0700

    show nav only when > 1

--- resources/views/components/commons/popup.blade.php
+++ resources/views/components/commons/popup.blade.php
@@ -1,7 +1,7 @@
 <div x-data="popupSlider()" class="overflow-hidden rounded-lg relative">
   <img :src="resolveImageUrl(items[currentIndex].image)" :alt="items[currentIndex].title"
     class="w-full h-full object-contain max-h-[80dvh] grid-pattern" />
-  <div class="w-full bottom-2 absolute flex justify-between px-2 gap-4 ">
+  <div class="w-full bottom-2 absolute flex justify-between px-2 gap-4" x-show="items.length > 1">
     <button @click="prev()" :disabled="currentIndex === 0" type="button"
       class="p-1 bg-white rounded-md background-blur-md  cursor-pointer shadow-md hover:bg-orange-600 group duration-200 active:scale-95">
       <x-icons.arrow-narrow-left class="text-slate-600 group-hover:text-white stroke-[1.5] h-6 w-auto" />
